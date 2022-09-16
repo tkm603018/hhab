518f6ad728dadba748738ad9feec2e15314d4406
“tkm603018” <tkm603018@gmail.com>
2022-09-16 23:10:11 +0900

    Created Category Model

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,35 @@
+class CategoriesController < ApplicationController
+  before_action :signed_in_user, only: [:index, :new, :create, :edit, :update, :destroy]
+  protect_from_forgery
+
+  def index
+    @subjects = current_user.categories.published.order("updated_at DESC")
+  end
+
+  def new
+  end
+
+  def create
+    current_user.categories.create(category_params)
+    redirect_to new_category_path, notice: '作成しました'
+  end
+
+  def edit
+    @subject = Category.find(params[:id])
+  end
+
+  def update
+    current_user.categories.find(params[:id]).update(category_params)
+    redirect_to edit_category_path, notice: '更新しました'
+  end
+
+  def destroy
+    current_user.categories.find(params[:id]).destroy
+    redirect_to categories_path, notice: '削除しました'
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:title, :path, :status)
+    end
+end
