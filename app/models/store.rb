06e8091d18b3691ead5e2a40ca0fc1ecd6707c88
“tkm603018” <tkm603018@gmail.com>
2022-09-23 23:31:30 +0900

    Fixed app/models/store.rb

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,7 +1,7 @@
 class Store < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, uniqueness: { scope: :user_id }, length: { minimum: 1, maximum: 30 }
-  has_many :items, foreign_key: 'title'
+  has_many :items, foreign_key: 'store_title'
 
   scope :by_store_like, lambda { |title|
     where('title LIKE :value', { value: "%#{title}%"})
